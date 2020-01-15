e02b1d6d81d3118ec79a991ddb457fd48fe081ed
tdmarkm <57206816+tdmarkm@users.noreply.github.com>
2020-01-15 22:33:05 +0800

    Update write_open_read_file.py

--- write_open_read_file.py
+++ write_open_read_file.py
@@ -2,19 +2,31 @@
 import teradatasql as tds
 import pandas as pd
 
-#query blocks
-query="""
-insert pua_test_db.testtable 
+hostn="192.168.170.128"
+usern="dbc"
+passw="dbc"
+
+
+#write block starts
+#query blocksstatement="""
+sel databasename, tablename from dbc.tables sample 10
 """
 
+with tds.connect(host=hostn,user=usern,password=passw) as sessionTD:
+    df=pd.read_sql(statement,sessionTD)
+
+data_output=df.to_csv(index=False, header=False) #suppress index column and the column header
+
+f=open('data.txt','w')
+f.write(data_output)
+f.close()
+#write block ends
+
+#read block starts
 df=pd.read_csv(r"data.txt")
 df=df.to_dict()  #makes it easier to handle the dataframe data into tables
 
 print(len(df['TableName'])) #max record
-
-hostn="192.168.170.128"
-usern="dbc"
-passw="dbc"
 
 results=[]
 with tds.connect(host=hostn,user=usern,password=passw) as sessionTD:
@@ -24,3 +36,4 @@ with tds.connect(host=hostn,user=usern,password=passw) as sessionTD:
             print(statement)
             curr.execute(statement)
         results+=curr.fetchall()  #this closes the cursor hence the placement
+#read block ends
