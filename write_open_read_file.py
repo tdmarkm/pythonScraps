b44aa42a2b59a984a00fd0f7b61d476f03755842
tdmarkm <57206816+tdmarkm@users.noreply.github.com>
2020-01-15 22:29:38 +0800

    Update write_open_read_file.py

--- write_open_read_file.py
+++ write_open_read_file.py
@@ -1,23 +1,26 @@
 
 import teradatasql as tds
 import pandas as pd
-import csv
+
+#query blocks
+query="""
+insert pua_test_db.testtable 
+"""
+
+df=pd.read_csv(r"data.txt")
+df=df.to_dict()  #makes it easier to handle the dataframe data into tables
+
+print(len(df['TableName'])) #max record
 
 hostn="192.168.170.128"
 usern="dbc"
 passw="dbc"
 
-#query blocks
-
-statement="""
-sel databasename, tablename from dbc.tables sample 10
-"""
-
+results=[]
 with tds.connect(host=hostn,user=usern,password=passw) as sessionTD:
-    df=pd.read_sql(statement,sessionTD)
-    
-data_output=df.to_csv(index=False, header=False) #suppress index column and the column header
-
-f=open('data.txt','w')
-f.write(data_output)
-f.close()
+    with sessionTD.cursor() as curr:
+        for x in range(0 , len(df['TableName'])):
+            statement = "insert into pua_test_db.testtable values ('"+df['DatabaseName'][x].strip()+"','"+df['TableName'][x].strip()+"')" 
+            print(statement)
+            curr.execute(statement)
+        results+=curr.fetchall()  #this closes the cursor hence the placement
