c3e03c09d758096befb72d6edefeb5d224e7f9b8
tdmarkm <57206816+tdmarkm@users.noreply.github.com>
2020-01-15 20:51:03 +0800

    Update write_open_read_file.py

--- write_open_read_file.py
+++ write_open_read_file.py
@@ -1,8 +1,23 @@
-f=open('he.txt','w')
-f.write('hahahah')
+
+import teradatasql as tds
+import pandas as pd
+import csv
+
+hostn="192.168.170.128"
+usern="dbc"
+passw="dbc"
+
+#query blocks
+
+statement="""
+sel databasename, tablename from dbc.tables sample 10
+"""
+
+with tds.connect(host=hostn,user=usern,password=passw) as sessionTD:
+    df=pd.read_sql(statement,sessionTD)
+    
+data_output=df.to_csv(index=False, header=False) #suppress index column and the column header
+
+f=open('data.txt','w')
+f.write(data_output)
 f.close()
-
-f=open('he.txt','r')
-for x in f:
-    print(x)
-
